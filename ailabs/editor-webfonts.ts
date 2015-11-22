fe3daae0035e346d61e2748c5be4ea1b7a2787ba
Sheyenne York <support@bpilotglobal.com>
2015-11-21 19:26:51 -0500

    Fixes fonts

--- ailabs/editor-webfonts.ts
+++ ailabs/editor-webfonts.ts
@@ -4,7 +4,7 @@ import CKEditor = require("/ZealandLib/TCKEditor/ckeditor/core/CKEDITOR_BOOTSTRA
 import FontFoundryClient = require("/Zealand/FontFoundry/FontFoundryClient"); // Used for dropdown and detecting use of fonts.
 
 // Set fonts.
-var NATIVE_FONTS = "Arial/Arial, Helvetica, sans-serif;Comic Sans MS/Comic Sans MS, cursive;Courier New/Courier New, Courier, monospace;Georgia/Georgia, serif;Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;Tahoma/Tahoma, Geneva, sans-serif;Times New Roman/Times New Roman, Times, serif;Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif";
+var NATIVE_FONTS = "Arial/Arial, Helvetica, sans-serif;Comic Sans MS/Comic Sans MS, Marker Felt, cursive;Courier New/Courier New, Courier, monospace;Georgia/Georgia, serif;Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;Tahoma/Tahoma, Geneva, sans-serif;Times New Roman/Times New Roman, Times, serif;Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif";
 
 var web_fonts = FontFoundryClient.fontsInstalled();
 
