f61c4cd2598f3b63297d8f212b27f1b3b6391c74
Sheyenne York <support@bpilotglobal.com>
2016-10-12 00:27:08 -0400

    CKEditor plugin working

---
+++ ailabs/plugins/personalize/plugin.js
@@ -0,0 +1,31 @@
+
+CKEDITOR.plugins.add("personalize",
+{
+  icons: "abbr",
+
+  init: function(editor)
+  {
+    function handleInsertCurly(field)
+    {
+      editor.insertText("{" + field.id + "}");
+    }
+
+    var command = new CKEDITOR.command(editor,
+      {
+        exec: function(editor)
+        {
+          PersonalizationDialog.openPrompt()
+          .affirmed(handleInsertCurly);
+        }
+      });
+
+    editor.addCommand("personalize", command);
+
+    editor.ui.addButton("Personalize", {
+      label: "Personalize",
+      command: "personalize",
+      toolbar: "insert"
+    });
+  }
+});
+
